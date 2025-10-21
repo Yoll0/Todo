daf5fac1c69a9e48981910d7b83c2225785b8fdd
W10 <d3550669@gmail.com>
2025-10-21 19:38:27 +0300

    Переместил код с прверкой на данные в класс

--- Wpf/Window1.xaml.cs
+++ Wpf/Window1.xaml.cs
@@ -59,66 +59,14 @@ namespace Wpf
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            string name = Name.Text;
-            string email = Email.Text;
-            string password = PassWord.Text.Trim();
-            string repassword = RePassWord.Text.Trim();
-//
-            if (name.Length >= 3)
+            if (proverka.ValidateInputs(Name, Email, PassWord, RePassWord))
             {
-                // Имя валидное
-                Name.BorderBrush = Brushes.Green;
-            }
-            else
-            {
-                // Имя невалидное
-                Name.BorderBrush = Brushes.Red;
-                return;
-            }
-//
-            Regex emailRegex = new Regex(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
-            if (emailRegex.IsMatch(email))
-            {
-                // Email валидный
-                Email.BorderBrush = Brushes.Green;
-            }
-            else
-            {
-                // Email невалидный
-                Email.BorderBrush = Brushes.Red;
-                return;
-            }
-
-            //
-
-            if (password.Length >= 6)
-            {
-                // Пароль валидный
-                PassWord.BorderBrush = Brushes.Green;
-            }
-            else
-            {
-                // Пароль невалидный
-                PassWord.BorderBrush = Brushes.Red;
-                return;
-            }
-            //
-            if (password != repassword)
-            {
-                MessageBox.Show("Пароли не совпадают");
-                return;
-            }
-
-
-            if (UR.UserRegistration(name, password, email))
-            {
-                Main_empty main_Empty = new Main_empty();
-                main_Empty.Show();
-                this.Close();
-            }
-            else
-            {
-                return;
+                if (UR.UserRegistration(Name.Text, PassWord.Text.Trim(), Email.Text))
+                {
+                    Main_empty main_Empty = new Main_empty();
+                    main_Empty.Show();
+                    this.Close();
+                }
             }
 
         }
---
+++ Wpf/proverka.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Wpf
+{
+    internal class proverka
+    {
+        public static bool ValidateInputs(TextBox nameTextBox, TextBox emailTextBox, TextBox passwordTextBox, TextBox repasswordTextBox)
+        {
+            string name = nameTextBox.Text;
+            string email = emailTextBox.Text;
+            string password = passwordTextBox.Text.Trim();
+            string repassword = repasswordTextBox.Text.Trim();
+
+            if (name.Length < 3)
+            {
+                nameTextBox.BorderBrush = Brushes.Red;
+                return false;
+            }
+            nameTextBox.BorderBrush = Brushes.Green;
+
+            Regex emailRegex = new Regex(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
+            if (!emailRegex.IsMatch(email))
+            {
+                emailTextBox.BorderBrush = Brushes.Red;
+                return false;
+            }
+            emailTextBox.BorderBrush = Brushes.Green;
+
+            if (password.Length < 6)
+            {
+                passwordTextBox.BorderBrush = Brushes.Red;
+                return false;
+            }
+            passwordTextBox.BorderBrush = Brushes.Green;
+
+            if (password != repassword)
+            {
+                MessageBox.Show("Пароли не совпадают");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
